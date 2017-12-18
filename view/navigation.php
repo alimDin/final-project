b80aed7d4312e295c8aed14b039d04e7ef7645fb
Alim Din <ad432@njit.edu>
2017-12-18 14:37:01 -0500

    static navigation

---
+++ view/navigation.php
@@ -0,0 +1,31 @@
+            <nav class="navbar navbar-fixed-top">
+            <div class="navbar-header">
+              <div class="search-field">
+                <input type="search" placeholder="Search..." class="form-control">
+              </div><a href="" class="navbar-brand text-hide"><img src="http://placehold.it/115x50" alt="Logo - Brand" width="115" height="50" class="img-responsive navbar-img"></a>
+              <button type="button" data-target="#menu" data-toggle="collapse" class="collapsed navbar-toggle"><span class="sr-only">Toggle navigation</span><span class="icon-bar"></span><span class="icon-bar"></span><span class="icon-bar"></span>
+              </button><a id="search-collapse" href="#" class="link-basic"><span class="fa fa-search"></span><span class="sr-only">Search</span></a>
+            </div>
+            <div id="menu" class="collapse navbar-collapse">
+              <ul class="nav navbar-nav navbar-right">
+                <li class="navbar-listitem"><a href="index.html" class="navbar-link">Home</a>
+                  
+                </li>
+                <li class="navbar-listitem"><a href="#" class="navbar-link">Design Ideas</a>
+                
+                </li>
+                <li class="navbar-listitem"><a href="#" class="navbar-link">Services</a>
+                 
+                </li>
+
+                <li class="navbar-listitem"><a href="#" class="navbar-link">Accounting</a>
+                  
+                </li>
+                <li class="navbar-listitem"><a href="#" class="navbar-link">Book A Free Consultation</a>
+                  
+                </li>
+                
+                <li class="navbar-listitem"><a href="login.php" class="navbar-link">Login</a>
+               
+
+                </li>
