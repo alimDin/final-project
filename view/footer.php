4afd9f0fb6adff381bc7dbb42a1f6e974653dc19
Alim Din <ad432@njit.edu>
2017-12-18 14:35:11 -0500

    footer

--- view/footer.php
+++ view/footer.php
@@ -1,7 +1,20 @@
 <footer>
-    <p class="copyright">
-        &copy; <?php echo date("Y"); ?> My Guitar Shop, Inc.
-    </p>
+   <div class="container">
+            <div class="row footer-row">
+              <div>
+                
+                <p class="phone">+1 732-421-8601</p><a href="mailto:info@patria.com" class="link-basic email">info@vonellitechnologies.com</a>
+              </div>
+              
+             
+            </div>
+            <div class="row footer-row"><small class="brand">© Vonelli Technologies 2017</small>
+              <ul class="pull-right list-inline">
+                <li><a href="https://www.facebook.com/Vonelli-Technologies-A-Website-App-Development-Company-311600282553666/" class="link-basic"><span class="sr-only">facebook</span><span class="fa fa-facebook"></span></a></li>
+ 
+              </ul>
+            </div>
+          </div>
 </footer>
 </body>
 </html>
