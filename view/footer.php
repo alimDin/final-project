b9ec40facba83b016eb7f5823cf6008dbe4cc961
Alim Din <ad432@njit.edu>
2017-12-19 02:53:46 -0500

    algined

--- view/footer.php
+++ view/footer.php
@@ -1,20 +1,14 @@
 <footer>
    <div class="container">
-            <div class="row footer-row">
-              <div>
+
                 
-                <p class="phone">+1 732-421-8601</p><a href="mailto:info@patria.com" class="link-basic email">info@vonellitechnologies.com</a>
-              </div>
-              
+                <p class="phone">+1 732-421-8601 - Contact@ad432@njit.edu</p>
+
+
              
             </div>
-            <div class="row footer-row"><small class="brand">© Vonelli Technologies 2017</small>
-              <ul class="pull-right list-inline">
-                <li><a href="https://www.facebook.com/Vonelli-Technologies-A-Website-App-Development-Company-311600282553666/" class="link-basic"><span class="sr-only">facebook</span><span class="fa fa-facebook"></span></a></li>
- 
-              </ul>
-            </div>
-          </div>
+           
+          
 </footer>
 </body>
 </html>
