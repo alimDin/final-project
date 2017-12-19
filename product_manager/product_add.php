c84e611f7aa30e03787a9ccbb9b70d251935a20c
Alim Din <ad432@njit.edu>
2017-12-19 02:36:24 -0500

    allfields

--- product_manager/product_add.php
+++ product_manager/product_add.php
@@ -1,6 +1,44 @@
-<?php include '../view/header.php'; ?>
+
+    <head>
+            <?php include '../view/header.php'; ?>
+
+  <link rel="stylesheet" href="/~ad432/week11/jquery/jquery-ui.css">
+  <script src="https://code.jquery.com/jquery-1.12.4.js"></script>
+  <script src="https://code.jquery.com/ui/1.12.1/jquery-ui.js"></script>
+     <script>
+    $( function() {
+    $( "#datepicker" ).datepicker();
+    } );
+ 
+
+    </script>
+    
+    </head>
+    
+    <header>
+          <?php include '../view/navigation.php' ; ?>
+    </header>
+
+
+    <style>
+
+
+    .container {
+    padding: 80px;
+    align-content: center;
+    }
+    </style>
+
+ 
 <main>
-    <h1>Add Product</h1>
+
+<section class="features-main container-fluid section-bordered">
+  <div class="container">
+    <h3>Add A Task</h3>
+
+  
+
+
     <form action="index.php" method="post" id="add_product_form">
         <input type="hidden" name="action" value="add_product">
 
@@ -13,26 +51,32 @@
         <?php endforeach; ?>
         </select>
         <br>
-
-        <label>Code:</label>
-        <input type="text" name="code" />
+        
+        <label>Date:</label>
+        <input type="text" id="datepicker" name="dateID"/>
         <br>
 
-        <label>Name:</label>
-        <input type="text" name="name" />
+        <label>Time:</label>
+        <input type="text" value="8:00pm" name="timeID"/>
+        <br>
+      
+        <label>Item:</label>
+        <input type="text"  name="item" />
         <br>
 
-        <label>List Price:</label>
-        <input type="text" name="price" />
+        <label>Description:</label>
+        <input type="text" name="message" />
         <br>
 
         <label>&nbsp;</label>
-        <input type="submit" value="Add Product" />
+        <input type="submit" value="Add Task" />
         <br>
     </form>
     <p class="last_paragraph">
         <a href="index.php?action=list_products">View Product List</a>
-    </p>
+    </p> 
+    </div>
+  </section>
 
 </main>
 <?php include '../view/footer.php'; ?>
