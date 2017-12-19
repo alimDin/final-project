17e9fb6f911c30b4da5d375a7c7c21b333ebf2dd
Alim Din <ad432@njit.edu>
2017-12-19 02:44:48 -0500

    delted menus

--- view/navigation.php
+++ view/navigation.php
@@ -2,30 +2,16 @@
             <div class="navbar-header">
               <div class="search-field">
                 <input type="search" placeholder="Search..." class="form-control">
-              </div><a href="" class="navbar-brand text-hide"><img src="http://placehold.it/115x50" alt="Logo - Brand" width="115" height="50" class="img-responsive navbar-img"></a>
+              </div><a href="index.php" class="navbar-brand text-hide"><img src="/~ad432/week11/img/todo.png" alt="Logo - Brand" width="115" height="50" class="img-responsive navbar-img"></a>
               <button type="button" data-target="#menu" data-toggle="collapse" class="collapsed navbar-toggle"><span class="sr-only">Toggle navigation</span><span class="icon-bar"></span><span class="icon-bar"></span><span class="icon-bar"></span>
               </button><a id="search-collapse" href="#" class="link-basic"><span class="fa fa-search"></span><span class="sr-only">Search</span></a>
             </div>
-            <div id="menu" class="collapse navbar-collapse">
+            
               <ul class="nav navbar-nav navbar-right">
-                <li class="navbar-listitem"><a href="index.html" class="navbar-link">Home</a>
-                  
-                </li>
-                <li class="navbar-listitem"><a href="#" class="navbar-link">Design Ideas</a>
-                
-                </li>
-                <li class="navbar-listitem"><a href="#" class="navbar-link">Services</a>
-                 
-                </li>
-
-                <li class="navbar-listitem"><a href="#" class="navbar-link">Accounting</a>
-                  
-                </li>
-                <li class="navbar-listitem"><a href="#" class="navbar-link">Book A Free Consultation</a>
-                  
-                </li>
-                
-                <li class="navbar-listitem"><a href="login.php" class="navbar-link">Login</a>
                
+                
+                <li class="navbar-listitem"><a href="login.php" class="navbar-link">Login</a></li>
 
-                </li>
+                  
+            </ul>
+            
