f9ef911b0086ee132c00fa2f00e3a607714e7fdf
Alim Din <ad432@njit.edu>
2017-12-19 02:42:28 -0500

    new header

--- view/header.php
+++ view/header.php
@@ -1,24 +1,15 @@
-<!DOCTYPE html>
-<html>
-
+ <title>Todo List</title>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <link rel="shortcut icon" href="media/img/favicon.png">
-    <link rel="stylesheet" href="css/bootstrap.min.css">
-    <link rel="stylesheet" href="css/hint.css">
-    <link rel="stylesheet" href="css/master.css">
-    <link rel="stylesheet" href="css/main.css">
+    <link rel="stylesheet" type="text/css" href="/~ad432/week11/css/bootstrap.min.css">
+    <link rel="stylesheet" type="text/css" href="/~ad432/week11/css/hint.css">
+    <link rel="stylesheet" type="text/css" href="/~ad432/week11/css/master.css">
+    <link rel="stylesheet" type="text/css" href="/~ad432/week11/css/main.css">
     <link href="https://fonts.googleapis.com/css?family=Open+Sans:400,300,700,800&amp;subset=latin,latin-ext" rel="stylesheet" type="text/css">
     <link href="https://fonts.googleapis.com/css?family=Lato:400,300,700,900" rel="stylesheet" type="text/css">
     <link href="https://fonts.googleapis.com/css?family=Montserrat:400,700" rel="stylesheet" type="text/css">
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.5.0/css/font-awesome.min.css">
-<head>
-    <title>My Guitar Shop</title>
-    <link rel="stylesheet" type="text/css"
-          href="/~ad432/week11/main.css">
-</head>
 
-<!-- the body section -->
-<body>
-<header><h1>My Guitar Shop</h1></header>
+
